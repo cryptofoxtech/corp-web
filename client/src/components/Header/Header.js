f05ddfd44c4e24dd4f45599b79734018fa8e0593
admin <admin@cryptofoxtech.com>
2025-05-30 00:13:09 -0400

    Feat: Implemented new global color palette and theme. Refined header dropdown and services page styling for improved aesthetics and usability. Ensured consistent button colors.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,42 +1,55 @@
 // client/src/components/Header/Header.js
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
-import './Header.css';
 
-import cryptofoxLogo from '../../assets/images/cryptofox-logo.png';
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
+import logo from '../../assets/images/cryptofox-logo.png';
 
 const Header = () => {
   return (
-    <Navbar expand="lg" data-bs-theme="dark" className="cryptofox-navbar">
+    <Navbar expand="lg" className="navbar">
       <Container>
-        <Navbar.Brand as={Link} to="/" className="cryptofox-brand">
+        <Navbar.Brand as={Link} to="/">
           <img
-            src={cryptofoxLogo}
+            src={logo}
             alt="Cryptofox Technology Ltd Logo"
-            className="cryptofox-logo"
-          />
-          <span className="cryptofox-company-name">Cryptofox Technology Ltd</span>
+            className="d-inline-block align-top"
+          />{' '}
+          <span>Cryptofox Technology Ltd</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
-
-            <NavDropdown title="Services" id="services-nav-dropdown">
+            
+            {/* START: Updated Services NavDropdown for clickability */}
+            <NavDropdown
+              title="Services"
+              id="basic-nav-dropdown"
+              // Add an onClick handler to navigate when the main 'Services' title is clicked
+              onClick={(e) => {
+                // Prevent default dropdown toggle behavior if a direct link is desired on title click
+                // For React-Bootstrap, often the title itself won't navigate without this or a custom component
+              }}
+            >
+              {/* This is the item that links to the main /services page */}
+              <NavDropdown.Item as={Link} to="/services">All Services</NavDropdown.Item>
+              <NavDropdown.Divider /> {/* Optional: Add a visual separator */}
               <NavDropdown.Item as={Link} to="/services/devops">DevOps Strategy</NavDropdown.Item>
               <NavDropdown.Item as={Link} to="/services/cloud">Cloud Computing</NavDropdown.Item>
               <NavDropdown.Item as={Link} to="/services/ai">AI Services</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item as={Link} to="/services/other">Other IT Services</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/services/webdev">Web Development</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/services/sysadmin">System Administration</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/services/blockchain">Blockchain Development</NavDropdown.Item>
             </NavDropdown>
+            {/* END: Updated Services NavDropdown */}
 
             <Nav.Link as={Link} to="/about">About Us</Nav.Link>
             <Nav.Link as={Link} to="/partners">Partners</Nav.Link>
             <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
-            {/* MODIFIED: Changed from Nav.Link to a regular <a> tag */}
-            <a href="tel:437-494-7222" className="nav-link phone-number">437-494-7222</a>
-            <Nav.Link as={Link} to="/quote" className="btn btn-primary get-a-quote-btn">Get a Quote</Nav.Link>
+            <Nav.Link href="tel:+14374947222">437-494-7222</Nav.Link>
+            <Button as={Link} to="/contact" className="btn-get-quote">Get a Quote</Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
